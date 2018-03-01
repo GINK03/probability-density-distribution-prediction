6e7d47bdbd4680a3d1aba6f1d7305d534b5310cb
Your Name <gim.kobayashi@gmail.com>
2018-03-01 20:12:38 +0900

    update

---
+++ distgen.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+SIZE = 500
+obs_freq = {}
+for i in range(SIZE):
+  sample1 = np.random.normal(loc=-2*np.sin(i/10), scale=1, size=10000)
+  sample2 = np.random.normal(loc=5*np.cos(i/10), scale=2, size=30000)
+
+  sample = np.append(sample1, sample2, axis=0)
+  for s in sample.tolist():
+    #print(s, s//0.001000)
+    obs = s//0.5000
+    if obs_freq.get(obs) is None:
+      obs_freq[obs] = [0]*SIZE
+    obs_freq[obs][i] += 1
+
+for obs, freq in sorted(obs_freq.items(), key=lambda x:x[0]):
+  print(obs, ' '.join(map(str, freq)))
